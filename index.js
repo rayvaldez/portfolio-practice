24b8158c40e91c8ac9d771aaf59edfb9b9f75356
rayvaldez <ray_valdez@hotmail.co.uk>
2022-02-18 23:32:40 +0000

    edit parallax settings

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const hrparallax = (distance, speed) => {
-  const motorbike = document.getElementById('motorbike')
-  motorbike.style.transform = "translateX("+ -distance*speed + "px)";
+const hrparallax = (image, distance, speed) => {
+  const item = document.getElementById(image)
+  item.style.transform = "translateX("+ -distance*speed + "px)";
+}
+
+const vrparallax = (distance, speed) => {
+  const developer = document.getElementById('developer')
+  developer.style.transform = "translateY("+ -distance*speed + "px)";
 }
 
 document.addEventListener('scroll', function() {
-  hrparallax(window.scrollY, -0.6);
+  hrparallax('motorbike', window.scrollY, 2.3);
+  hrparallax('camera', window.scrollY, -0.5);
+  vrparallax(window.scrollY, 0.2)
 });
