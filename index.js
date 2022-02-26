a83d120cbbddbabf34cd5af2c5b2e2428e710895
rayvaldez <ray_valdez@hotmail.co.uk>
2022-02-26 15:58:29 +0000

    Resize Project images for better UI

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const hrparallax = (image, distance, speed) => {
-  const item = document.getElementById(image)
-  item.style.transform = "translateX("+ -distance*speed + "px)";
-}
 
-const vrparallax = (distance, speed) => {
-  const developer = document.getElementById('developer')
-  developer.style.transform = "translateY("+ -distance*speed + "px)";
-}
-
-document.addEventListener('scroll', function() {
-  hrparallax('motorbike', window.scrollY, 2.3);
-  hrparallax('camera', window.scrollY, -0.5);
-  vrparallax(window.scrollY, 0.2)
-});
